17cf02199d0f9cea7383b2cdc87e760135945bb2
Muhammed Ashique <muhammedashiques2017@gmail.com>
2023-01-13 10:56:38 +0530

    just basic database created with mongo db

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -2,13 +2,16 @@ import express from 'express';
 
 let articlesInfo = [{
     name: 'learn-react',
-    upvotes: 0
+    upvotes: 0,
+    comments: []
 }, {
     name: 'learn-node',
-    upvotes: 0
+    upvotes: 0,
+    comments: []
 }, {
     name: 'mongodb',
-    upvotes: 0
+    upvotes: 0,
+    comments: []
 }];
 
 const app = express();
@@ -27,6 +30,23 @@ app.put('/api/articles/:name/upvote', (req, res) => {
     }
 
 });
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    console.log("hello")
+    const { name } = req.params;
+    console.log(name)
+    const { postedBy, text } = req.body;
+    const article = articlesInfo.find(a => a.name === name);
+    if (article) {
+        article.comments.push({postedBy, text})
+        res.send(article)
+        
+    } else{
+        res.send('That article doesn\'t exist')
+    }
+
+
+})
 
 app.listen(80, () => {
     console.log('Server is listening on port 80');
